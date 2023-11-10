bfa50ac01963196aebd4a478f384c6c627f51c4e
Franco Policrite <120028970+policrais@users.noreply.github.com>
2023-11-10 15:03:23 -0300

    s

--- Inteligencia Artificial/Decision Threes.py
+++ Inteligencia Artificial/Decision Threes.py
@@ -1,11 +1,17 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 import matplotlib.pyplot as plt
+
+# Cargar el conjunto de datos de iris
 iris = load_iris()
 X = iris.data
 y = iris.target
+
+# Crear y entrenar el clasificador de árbol de decisión con profundidad máxima de 2
 clf = DecisionTreeClassifier(max_depth=2, random_state=42)
 clf.fit(X, y)
+
+# Visualizar el árbol de decisión
 plt.figure(figsize=(8, 5))
 plot_tree(clf, filled=True, feature_names=iris.feature_names, class_names=iris.target_names)
 plt.show()
